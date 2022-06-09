e6966883e12842345f4ee49da386c56931e0dca8
David F <flo22001@byui.edu>
2022-06-08 23:40:09 -0400

    added comments to objects

--- greed/game/gem.py
+++ greed/game/gem.py
@@ -1,14 +1,24 @@
-"""
-Import moving_objects.py
-This class will draw the gems for the game
-Inherit from moving_object
-"""
 from game.moving_object import MovingObject
+
 class Gem(MovingObject):
+    """
+    Represents the rock (o) objecs
+    Inherits from MovingObject
+
+    Attributes:
+        _points_value (int): Points when object matches position of player
+    """
     def __init__(self):
+        """Constructs a new Rock object.
+        """
         super().__init__()
         super().set_text("*")
+        self._points_value = 1
 
     def get_points(self):
-        # return self._message
-        return 1
+        """Returns points value of the object.
+        
+        Returns:
+            points (int): Points value of the object if intersected by player.
+        """
+        return self._points_value
--- greed/game/rock.py
+++ greed/game/rock.py
@@ -1,20 +1,23 @@
-"""
-Draws the rock (o)
-Inherits from moving_objects
-"""
-
-
 from game.moving_object import MovingObject
 
-
 class Rock(MovingObject):
+    """
+    Represents the rock (o) objecs
+    Inherits from MovingObject
+
+    Attributes:
+        _points_value (int): Points when object matches position of player
+    """
     def __init__(self):
+        """Constructs a new Rock object.
+        """
         super().__init__()
         super().set_text("o")
-
+        self._points_value = -1
 
     def get_points(self):
-        # return self._message
-        return -1
-
+        """        Returns:
+            points (int): Points value of the object if intersected by player.
+        """
+        return self._points_value
     
